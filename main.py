38607fe93b6d5e6f46b50f954d84446009bd0b3f
superznake <superznake@yandex.ru>
2024-12-13 16:04:24 +0300

    .

--- main.py
+++ main.py
@@ -1,26 +1,21 @@
 from owm_key import owm_api_key
-import json
-from urllib import request
-from getweatherdata import get_weather_data
+import requests
 
-# TODO 1
-def get_weather_data(place, api_key=None):
 
-    # использовать библиотеку requests вместо urllib
-    with request.urlopen(
-            f'https://api.openweathermap.org/data/2.5/weather?q={place}&appid={api_key}'
-    ) as f:
-        res = f.read().decode('utf-8')
+def get_weather_now(place, api_key=None):
+    params = {"q": place, "appid": api_key, "units": "metric", "lang": "ru"}
+    r = requests.get("https://api.openweathermap.org/data/2.5/weather", params=params)
+    result = r.json()
+    print(result)
 
-        res_obj = json.loads(res)
 
-    # print(res_json)
-
-    print((res_obj))
-
-    # with open('data.json', 'w') as f:
-    #     json.dump(res_json, f, ensure_ascii=False, indent=4)
+def get_weather_forecast(place, api_key=None):
+    params = {"q": place, "appid": api_key, "units": "metric", "lang": "ru"}
+    r = requests.get("https://api.openweathermap.org/data/2.5/forecast", params=params)
+    result = r.json()
+    print(result)
 
 
 if __name__ == '__main__':
-    get_weather_data('Moscow', api_key=owm_api_key)
+    get_weather_now('Moscow', api_key=owm_api_key)
+    get_weather_forecast('Moscow', api_key=owm_api_key)
